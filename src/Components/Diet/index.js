1c63acf0eaef34a7a5d9cf40717ce87586ed38ea
Ahmet Kutuk <ahmetcankutk@gmail.com>
2021-06-12 02:57:37 +0300

    diet compoment edited

--- src/Components/Diet/index.js
+++ src/Components/Diet/index.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import {Wrapper} from '../../styles/pages/Diet';
+import React from "react";
+import { Wrapper, DıetImg, DıetContainer, Info, ReCalculate } from "../../styles/pages/Diet";
+import DietPıc from "../../assets/undraw_diet_ghvw.svg";
+import {Link} from 'react-router-dom';
 function Diet() {
-    return (
-        <>
-            <Wrapper>
-                Diet component will be updated
-            </Wrapper> 
-        </>
-    )
+  return (
+    <>
+      <Wrapper>
+        <DıetImg alt="" src={DietPıc} />
+        <DıetContainer>
+          <span className="title">Your Dıet</span>
+          <Info>
+            <span>Morning:</span>1 slice of wholemeal bread, 1 feta cheese,
+            plenty of tomato, cucumber 1 piece of fruit.
+          </Info>
+          <Info>
+            <span>Lunch:</span>100gr lean meat salad, 1 bowl of light yogurt, 1
+            bowl of soup.
+          </Info>
+          <Info>
+            <span>Evening:</span>meatless dinner, 1 whole wheat bread salad, 1
+            bowl of light yogurt Break 1 hit fruit
+          </Info>
+          <Link to="/"><ReCalculate>Re Calculate</ReCalculate></Link>
+        </DıetContainer>
+      </Wrapper>
+    </>
+  );
 }
 
-export default Diet
+export default Diet;
--- src/styles/pages/Diet.js
+++ src/styles/pages/Diet.js
@@ -4,12 +4,12 @@ export const Wrapper = styled.div`
     height:70%px;
     background-color:white;
     position:absolute;
-    left:40px;
+    left:200px;
     border-radius:25px;
     position:relative;
     display:flex;
     flex-direction:column;
-    padding:170px 150px 170px 150px ;
+    padding:120px 180px 120px 170px ;
     -webkit-box-shadow: 5px 5px 50px 5px #AF67E9; 
     box-shadow: 5px 5px 50px 5px #AF67E9;
     overflow:hidden;
@@ -26,4 +26,72 @@ export const Wrapper = styled.div`
 
     }
     
+`;
+
+export const DıetImg = styled.img`
+display:none;
+   @media (min-width: 768px) {
+     padding-left:15px;
+     display:block;
+   }
+
+`;
+
+export const DıetContainer = styled.div`
+  display:flex;
+  flex-direction:column;
+  margin-right:10px;
+  padding-right:80px;
+  
+  @media (min-width: 768px) {
+    display:flex;
+    flex-direction:column;
+    margin-left:50px;
+    padding-right:100px;
+    position:static;
+  
+
+    }
+`;
+
+export const Info = styled.p`
+  color:#6565e7;
+  margin-top:30px;
+  position:relative;
+  right:130px;
+  width:300px;
+
+  @media (min-width: 768px) {
+    position:static;
+  }
+
+  span{
+    font-weight:700;
+  }
+
+`;
+
+export const ReCalculate = styled.button`
+  
+  background:#6565e7;
+  width:300px;
+  position:relative;
+  right:150px;
+  border-radius:0.5rem;
+  color:white;
+  font-weight:700;
+  padding:1rem;
+  margin-top:50px;
+
+  &:hover{
+        box-shadow:0px 5px 15px 0px #AF67E9;
+    }
+
+    @media (min-width: 768px) {
+      width:100%;
+      height:48px;
+      position:static;
+      margin-top:30px;
+    }
+
 `;
